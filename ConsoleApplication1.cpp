0914055aadb430d6d0f40b2ab13c8543e19e4977
gary <ying050524@gmail.com>
2024-05-08 19:44:29 +0100

    not correct

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -26,14 +26,60 @@ using namespace std;
 
 
 
-int solution(vector<int>& A) {
+int solution1(vector<int>& A) {
     // Implement your solution here
-    sort(A.begin(), A.end());
-    int len = A.size();
-    for (int i = 0; i <= len - 3; i++) {
-        if (long(A[i]) + long(A[i + 1]) > long(A[i + 2])) {
-            return 1;
+    int count = 0;
+    const int N = A.size();
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (abs(i - j) <= A[i] + A[j]) {
+                count++;
+            }
         }
     }
-    return 0;
+    return count;
 }
+
+
+int solution(vector<int>& A) {
+    // Implement your solution here
+    int count = 0;
+    vector<vector<int>> sortSec;
+    for (int i = 0; i < A.size(); i++) {
+        sortSec.push_back({i - A[i], i + A[i] });
+    }
+    sort(sortSec.begin(), sortSec.end());
+    auto it = sortSec.begin();
+    for (int i = 0; i < sortSec.size(); i++) {
+        int end = sortSec[i][1];
+        auto next = upper_bound(it + i, sortSec.end(), end,
+            [](const vector<int>& a, const int b)
+            {return a[0] < b; });
+
+        int temp = distance(next, it + i);
+        count = count + (temp - 1);
+    }
+    return count;
+}
+
+
+int solution2(vector<int>& A) {
+    int count = 0;
+    vector<vector<int>> sortSec;
+    for (int i = 0; i < A.size(); i++) {
+        sortSec.push_back({ i - A[i], i + A[i] });
+    }
+    sort(sortSec.begin(), sortSec.end());
+    auto it = sortSec.begin();
+    for (int i = 0; i < sortSec.size(); i++) {
+        int end = sortSec[i][1];
+        auto next = upper_bound(it, sortSec.end(), end,
+            [](const vector<int>& a, const int& b)
+            { return a[0] < b; }); // Compare based on the first element of vector
+
+        int temp = distance(it, next); // Calculate the number of elements less than end
+        count += temp; // Add temp, as it represents the number of intervals that intersect with the current interval
+    }
+    return count;
+}
+
