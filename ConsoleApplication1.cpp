e02220b56d7f92962d06e84ce8a3f82e79a84460
gary <ying050524@gmail.com>
2024-05-08 20:41:55 +0100

    arithmeic failure

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -44,6 +44,15 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
+#include <map>
+#include <vector>
+#include <set>
+#include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
 
 int solution(vector<int>& A) {
     const auto MAX_INTERSECTIONS = 10000000;
@@ -55,11 +64,11 @@ int solution(vector<int>& A) {
     // reserve the memory for all the vectors to avoid many allocations
     segments.reserve(v_size);
 
-    // Add the pairs to the array
     for (int i = 0; i < v_size; ++i) {
         int radius = A[i];
-        int begin = max(0, static_cast<int>(i) - radius);
-        int end = min(v_size - 1, i + radius);
+        // Calculate begin and end while checking for overflow
+        int begin = max(0, i - radius >= 0 ? i - radius : 0);
+        int end = min(v_size - 1, i + radius < v_size ? i + radius : v_size - 1);
         segments.push_back({ begin, end });
     }
 
@@ -67,20 +76,11 @@ int solution(vector<int>& A) {
     sort(segments.begin(), segments.end());
 
     for (int i = 0; i < v_size; ++i) {
-        // for each disk in order of the *starting* position of the disk, not the centre
-        // find the end position of that disk from the array of vectors
         int disk_end = segments[i][1];
-        // find the index of the rightmost value less than or equal to the interval-end
-        // this finds the number of disks that have started before disk i ends
         auto it = upper_bound(segments.begin(), segments.end(), vector<int>{disk_end, 0},
             [](const vector<int>& lhs, const vector<int>& rhs) -> bool { return lhs[0] < rhs[0]; });
 
         int count = distance(segments.begin(), it);
-
-        // subtract current position to exclude previous matches
-        // for disk i, i disks that start to the left have already been dealt with
-        // subtract i from count to prevent double counting
-        // subtract one more to prevent counting the disk itself
         count -= (i + 1);
         intersections += count;
         if (intersections > MAX_INTERSECTIONS) {
