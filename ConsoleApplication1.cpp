d144b023659eb91c2ed5c1bdb6787588318c622e
gary <ying050524@gmail.com>
2024-05-08 15:54:03 +0100

    GenomicRangeQuery

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -19,18 +19,49 @@ using namespace std;
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
-
-
-int solution(int A, int B, int K) {
-    // Implement your solution here
-    int numA = A / K;
-    int numB = B / K;
-    // how about numA itself
-    int ret = numB - numA;
-    if (A % K == 0) { // should include A itself
-        ret = ret + 1;
+bool contains(vector<int> pos, int s, int e) {
+    auto lowit = lower_bound(pos.begin(), pos.end(), s);
+    if (lowit == pos.end()) {
+        return false;
     }
-    return ret;
+
+    if (*lowit <= e) {
+        return true;
+    }  else {
+        return false;
+    }
+}
+
+vector<int> solution(string& S, vector<int>& P, vector<int>& Q) {
+ 
+    // Implement your solution here
+    vector<int> charPos[4];
+    for (int i = 0; i < S.size(); i++) {
+        char cur = S[i];
+        if (cur == 'A') {
+            charPos[0].push_back(i);
+        } else if (cur == 'C') {
+            charPos[1].push_back(i);
+        } else if (cur == 'G') {
+            charPos[2].push_back(i);
+        } else if (cur == 'T') {
+            charPos[3].push_back(i);
+        }
+    }
+
+    vector<int> result;
+    for (int i = 0; i < P.size(); i++) {
+        int s = P[i];
+        int e = Q[i];
+        int factor = INT_MAX;
+        for (int j = 3; j >= 0; j--) {
+            if (contains(charPos[j], s, e)) {
+                factor = min(factor, j+1); // j+1 is the actual factor 1-4
+            }
+        }
+        result.push_back(factor);
+    }
+    return result;
 }
 
 
