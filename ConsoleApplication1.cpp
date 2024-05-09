c032ac5e951ee83e4f8b28049bf51e3d66712d2a
gary <ying050524@gmail.com>
2024-05-09 10:13:31 +0100

    stone wall fix

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -68,6 +68,24 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <iostream>
+using namespace std;
+
 int solution(vector<int>& H) {
     // Implement your solution here
     stack<int> myStack;
@@ -83,20 +101,19 @@ int solution(vector<int>& H) {
             }
             else if (myStack.top() == H[i]) {
                 continue;
-            } else {
-                bool keep = true;
+            }
+            else {
+
                 while (myStack.empty() == false) {
                     int top = myStack.top();
                     if (top > H[i]) {
                         myStack.pop();
                         count++;
-                        cout << "top= " << top << "count= " << count << "cur=" << H[i] << endl;
+                        //cout << "top= " << top << "count= " << count << "cur=" << H[i] << endl;
                     }
                     else if (top == H[i]) {
-                        myStack.pop();
-                        count++;
-                        keep = false;
-                        cout << "top= " << top << "count= " << count << "cur=" << H[i] << endl;
+                        break; // drop cur
+                        //cout << "top= " << top << "count= " << count << "cur=" << H[i] << endl;
                         break;
                     }
                     else { // top < H[i]
@@ -104,13 +121,39 @@ int solution(vector<int>& H) {
                         break;
                     }
                 }
-                if (myStack.empty() == true && keep == true) {
+                if (myStack.empty() == true) {
                     myStack.push(H[i]);
                 }
+
             }
         }
     }
-    cout << "myStack.size()= " << myStack.size() << "count= " << count << endl;
-    cout << "top= " << myStack.top() << endl;
+    //cout << "myStack.size()= " << myStack.size() << "count= " << count << endl;
+    //cout << "top= " << myStack.top() << endl;
     return count + myStack.size();
+}
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+int solution(std::vector<int>& H) {
+    std::stack<int> myStack;
+    int count = 0;
+    for (int height : H) {
+        while (!myStack.empty() && myStack.top() > height) {
+            myStack.pop();
+            count++;
+        }
+        if (myStack.empty() || myStack.top() < height) {
+            myStack.push(height);
+        }
+    }
+    return count + myStack.size();
+}
+
+int main() {
+    std::vector<int> H = { 8, 8, 5, 7, 9, 8, 7, 4, 8 };
+    std::cout << "Minimum number of blocks needed: " << solution(H) << std::endl;
+    return 0;
 }
