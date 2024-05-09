100721acccac7ae54b489f7e14fda3fd2a1b99d2
gary <ying050524@gmail.com>
2024-05-09 08:48:21 +0100

    fish

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -68,30 +68,34 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
-
-
-#include <vector>
-#include <algorithm>
-
-int solution(std::vector<int>& A) {
+int solution(vector<int>& A, vector<int>& B) {
+    // Implement your solution here
+    const int len = A.size();
+    stack<int> myStack;
     int count = 0;
-    std::vector<std::vector<long long>> sortSec;
-    for (size_t i = 0; i < A.size(); i++) {
-        long long start = static_cast<long long>(i) - static_cast<long long>(A[i]);
-        long long end = static_cast<long long>(i) + static_cast<long long>(A[i]);
-        sortSec.push_back({ start, end });
-    }
-    std::sort(sortSec.begin(), sortSec.end());
-    auto it = sortSec.begin();
-    for (unsigned int i = 0; i < sortSec.size(); i++) {
-        long long end = sortSec[i][1];
-        auto next = std::upper_bound(it + i, sortSec.end(), std::vector<long long>{end, 0},
-            [](const std::vector<long long>& lhs, const std::vector<long long>& rhs) -> bool { return lhs[0] < rhs[0]; });
-
-        int temp = static_cast<int>(std::distance(it + i, next));
-        if (temp > 1) {
-            count = count + (temp - 1);
+    for (int i = 0; i < len; i++) {
+        if (B[i] == 1) { // push to stack donw stream
+            myStack.push(A[i]);
+        }
+        else { // up stream
+            if (myStack.empty()) {
+                count++;
+            }
+            else {
+                while (myStack.empty() == false) {
+                    int top = myStack.top();
+                    if (top > A[i]) {
+                        break;
+                    }
+                    else {
+                        myStack.pop();
+                    }
+                }
+                if (myStack.empty()) {
+                    count++;
+                }
+            }
         }
     }
-    return count;
+    return count + myStack.size();
 }
