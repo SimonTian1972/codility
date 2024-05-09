a0e87d7fe1f4af6afbd09c5a70a1d88955edb4ea
gary <ying050524@gmail.com>
2024-05-09 13:43:07 +0100

    MaxProfit

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -109,51 +109,49 @@ using namespace std;
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
 int solution(vector<int>& A) {
     // Implement your solution here
     const int N = A.size();
-    vector<int> leftLeader(N, -1);
-    vector<int> rightLeader(N, -1);
-    map<int, int> leftCount; // num, count
-    map<int, int> rightCount;
-
-    int largest = 0;
+    vector<int> leftMin(N, 0);
+    vector<int> rightMax(N, 0);
+    int minV = INT_MAX;
+    //cout << "N=" << N << " " << leftMin.size() << " " << rightMax.size() << endl;
     for (int i = 0; i < N; i++) {
-        leftCount[A[i]]++;
-        if (leftCount[A[i]] > largest) {
-            largest = leftCount[A[i]];
-            if (largest > (i + 1) / 2) {
-                leftLeader[i] = A[i];
-            }
-        } else {
-            if (largest > (i + 1) / 2) {
-                leftLeader[i] = leftLeader[i-1];
-            }
-        }
-    } // leftLeader
-
-    largest = 0;
-    for (int i = N - 1; i >= 0; i--) {
-        rightCount[A[i]]++;
-        if (rightCount[A[i]] > largest) {
-            largest = rightCount[A[i]];
-            if (largest > (N-i) / 2) {
-                rightLeader[i] = A[i];
-            }
-        } else {
-            if (largest > (N-i) / 2) {
-                rightLeader[i] = rightLeader[i+1];
-            }
-        }
-    }// rightLeader
-
-    int count = 0;
-    for (int i = 0; i < N - 1; i++) {
-        int j = i + 1;
-        if (leftLeader[i] == rightLeader[j] && leftLeader[i] != -1) {
-            count++;
-        }
+        minV = min(minV, A[i]);
+        leftMin[i] = minV;
     }
-    return count;
+    //cout << "ok1" << endl;
+    int maxV = INT_MIN;
+    for (int i = N - 1; i >= 0; i--) {
+        //cout << "ok2 i =" << i << endl;
+        maxV = max(maxV, A[i]);
+        rightMax[i] = maxV;
+    }
+    //cout << "ok2b" << endl;
+    int profit = 0;
+    //cout << "ok3" << endl;
+    for (int i = 0; i < N - 1; i++) {
+        int low = leftMin[i];
+        int high = rightMax[i + 1];
+        profit = max(profit, high - low);
+    }
+    return profit;
 }
-
