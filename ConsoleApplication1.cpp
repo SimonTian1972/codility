fbf36db493bf6a73c4c47e7c6d32cf68f4e16855
gary <ying050524@gmail.com>
2024-05-09 11:08:23 +0100

    leader number 2

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -88,30 +88,7 @@ using namespace std;
 
 
 
-int solution(vector<int>& A) {
-    // Implement your solution here
-    unordered_map<int, vector<int>> myMap; // num, count
-    for (int i = 0; i < A.size(); i++) {
-        myMap[A[i]].push_back(i);
-    }
 
-    int largest = 0;
-    int idx = -1;
-    for (auto myPair : myMap) {
-        if (myPair.second.size() > largest) {
-            largest = myPair.second.size();
-            idx = myPair.second[0];
-        }
-    }
-
-    const int N = A.size();
-    if (largest > N / 2) {
-        return idx;
-    }
-    else {
-        return -1;
-    }
-}
 
 // cout << "this is a debug message" << endl;
 #include <map>
@@ -126,28 +103,57 @@ int solution(vector<int>& A) {
 #include <iostream>
 using namespace std;
 
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
 int solution(vector<int>& A) {
     // Implement your solution here
-    unordered_map<int, vector<int>> myMap; // num, count
-    for (unsigned int i = 0; i < A.size(); i++) {
-        myMap[A[i]].push_back(i);
-    }
+    const int N = A.size();
+    vector<int> leftLeader(N, -1);
+    vector<int> rightLeader(N, -1);
+    map<int, int> leftCount; // num, count
+    map<int, int> rightCount;
 
-    unsigned int largest = 0;
-    int idx = -1;
-    for (auto myPair : myMap) {
-        if (myPair.second.size() > largest) {
-            largest = myPair.second.size();
-            idx = myPair.second[0];
+    int largest = 0;
+    for (int i = 0; i < N; i++) {
+        leftCount[A[i]]++;
+        if (leftCount[A[i]] > largest) {
+            largest = leftCount[A[i]];
+            if (largest > (i + 1) / 2) {
+                leftLeader[i] = A[i];
+            }
+        } else {
+            if (largest > (i + 1) / 2) {
+                leftLeader[i] = leftLeader[i-1];
+            }
+        }
+    } // leftLeader
+
+    largest = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        rightCount[A[i]]++;
+        if (rightCount[A[i]] > largest) {
+            largest = rightCount[A[i]];
+            if (largest > (N-i) / 2) {
+                rightLeader[i] = A[i];
+            }
+        } else {
+            if (largest > (N-i) / 2) {
+                rightLeader[i] = rightLeader[i+1];
+            }
+        }
+    }// rightLeader
+
+    int count = 0;
+    for (int i = 0; i < N - 1; i++) {
+        int j = i + 1;
+        if (leftLeader[i] == rightLeader[j] && leftLeader[i] != -1) {
+            count++;
         }
     }
-
-    const unsigned int N = A.size();
-    if (largest > N / 2) {
-        return idx;
-    }
-    else {
-        return -1;
-    }
+    return count;
 }
 
