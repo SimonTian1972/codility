3c12e411552fc46bf0e67f0d10989e77ad05834f
gary <ying050524@gmail.com>
2024-05-10 18:56:30 +0100

    NumbeerSolitarie

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -12,47 +12,9 @@ using namespace std;
 
 // you can use includes, for example:
 // #include <algorithm>
-int solution(vector<int>& A, vector<int>& B, vector<int>& C) {
-    // Implement your solution here
-    set<vector<int>> myPlank;
-    const int N = A.size();
-    for (int i = 0; i < N; i++) {
-        myPlank.insert(vector<int>{A[i], B[i]});
-    }
-
-    const int cN = C.size();
-    for (int i = 0; i < cN; i++) {
-        auto itEnd = upper_bound(myPlank.begin(), myPlank.end(), vector<int>{C[i], 0},
-            [](vector<int>left, vector<int>right) {
-                return left[0] < right[0];
-            });
-        vector<vector<int>> toRemove;
-        for (auto it = myPlank.begin(); it != myPlank.end() && it != itEnd; it++) {
-            // save item to remove
-            int end = (*it)[1];
-            if (end >= C[i]) {
-                toRemove.push_back(vector<int>{(*it)[0], (*it)[1]});
-            }
-        }
-        // remove items
-        for (auto item : toRemove) {
-            myPlank.erase(item);
-        }
-        if (myPlank.empty()) {
-            return i+1; // number of nails
-        }
-    }
-    return -1;
-}
-
-// you can use includes, for example:
-// #include <algorithm>
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
-
-// you can use includes, for example:
-// #include <algorithm>
 #include <map>
 #include <unordered_map>
 #include <vector>
@@ -65,111 +27,44 @@ int solution(vector<int>& A, vector<int>& B, vector<int>& C) {
 #include <iostream>
 using namespace std;
 
-/*
-N is an integer within the range [0..30,000];
-each element of arrays A and B is an integer within the range [0..1,000,000,000];
-A[I] ≤ B[I], for each I (0 ≤ I < N);
-B[K] ≤ B[K + 1], for each K (0 ≤ K < N − 1).
-*/
-
-#include <vector>
-#include <algorithm>
-
-// notice that only "adjacent ropes" can be tied
-      // so, the problem is simple; we can use "greedy" method
-
-
-package Codility.Lesson14;
-
-import java.util.*;
-
-public class NailingPlanks {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a1 = { 1, 4, 5, 8 };
-		int[] a2 = { 4, 5, 9, 10 };
-		int[] a3 = { 4, 6, 7, 10, 2 };
-		int result1 = solution(a1, a2, a3);
-		System.out.println(result1);
-	}
-
-	public static int solution(int[] A, int[] B, int[] C) {
-		// the main algorithm is that getting the minimal index of nails which
-		// is needed to nail every plank by using the binary search
-		int N = A.length;
-		int M = C.length;
-		// two dimension array to save the original index of array C
-		int[][] sortedNail = new int[M][2];
-		for (int i = 0; i < M; i++) {
-			sortedNail[i][0] = C[i];
-			sortedNail[i][1] = i;
-		}
-		// use the lambda expression to sort two dimension array
-		Arrays.sort(sortedNail, (int x[], int y[])->x[0] - y[0]);
-		int result = 0;
-		for (int i = 0; i < N; i++) {// find the earlist position that can nail each plank, and the max value for all
-										// planks is the result
-			result = getMinIndex(A[i], B[i], sortedNail, result);
-			if (result == -1)
-				return -1;
-		}
-		return result + 1;
-	}
-
-	// for each plank , we can use binary search to get the minimal index of the
-	// sorted array of nails, and we should check the candidate nails to get the
-	// minimal index of the original array of nails.
-	public static int getMinIndex(int startPlank, int endPlank, int[][] nail, int preIndex) {
-		int min = 0;
-		int max = nail.length - 1;
-		int minIndex = -1;
-		while (min <= max) {
-			int mid = (min + max) / 2;
-			if (nail[mid][0] < startPlank)
-				min = mid + 1;
-			else if (nail[mid][0] > endPlank)
-				max = mid - 1;
-			else {
-				max = mid - 1;
-				minIndex = mid;
-			}
-		}
-		if (minIndex == -1)
-			return -1;
-		int minIndexOrigin = nail[minIndex][1];
-		// find the smallest original position of nail that can nail the plank
-		for (int i = minIndex; i < nail.length; i++) {
-			if (nail[i][0] > endPlank)
-				break;
-			minIndexOrigin = Math.min(minIndexOrigin, nail[i][1]);
-			// we need the maximal index of nails to nail every plank, so the
-			// smaller index can be omitted
-			if (minIndexOrigin <= preIndex)
-				return preIndex;
-		}
-		return minIndexOrigin;
-	}
+int minSum(vector<int>& A, int first, int nextPos) {
+    int len = nextPos - first + 1;
+    vector<int> dp(len, 0);
+    for (int i = first; i <= nextPos; i++) {
+        int idx = i - first;
+        int maxCur = INT_MIN;
+        for (int step = 1; step <= 6; step++) {
+            int prev = 0;
+            if (idx - step >= 0) {
+                prev = dp[idx - step];
+            }
+            maxCur = max(maxCur, prev + A[i]);
+        }
+        dp[idx] = maxCur;
+    }
+    return dp[len - 1] - A[nextPos];// do not count nextPos value
 }
 
-
-
-def ok(A, B, C, mid) :
-	# count values in C
-	cnt = [0] * (2 * len(C) + 1)
-	for i in C[:mid + 1]: cnt[i] += 1
-
-		# prefix sum : accumulate sum of cnt
-		for i in range(1, len(cnt)) : cnt[i] += cnt[i - 1]
-# if sum between A[i]~B[i] is 0, then plank i can not be covered
-			for a, b in zip(A, B) :
-				if cnt[b] == cnt[a - 1] : return False
-					return True
-
-This is a function named ok that checks if it's possible to nail all planks using a certain number of nails up to the mid-th nail in the list C. It returns True if it's possible, and False otherwise.
-A, B, and C are lists representing the start positions, end positions, and nail positions respectively.
-It initializes a list cnt with zeros, with a length of 2 * len(C) + 1. This list is used to count how many nails are present between each position.
-It iterates through the first mid + 1 elements of list C and increments the count in cnt for each nail position encountered.
-It then performs prefix sum computation on cnt to get cumulative counts.
-It iterates through pairs of elements from lists Aand B simultaneously using zip, and checks whether the sum of counts of nails between positions aand b is equal.If it is, it means a plank cannot be covered, and the function returns False.
-If the loop completes without finding any plank that cannot be covered, the function returns True.
+int solution(vector<int>& A) {
+    // Implement your solution here
+    const int N = A.size();
+    int idx = 0;
+    int sum = 0;
+    while (idx < N) {
+        if (idx == 0 || idx == N - 1 || A[idx] >= 0) {
+            sum = sum + A[idx];
+            idx++;
+            continue;
+        }
+        else { // A[idx] < 0
+            int nextPos = idx;
+            while (A[nextPos] < 0 && nextPos != N - 1) {
+                nextPos++;
+            }
+            int temp = minSum(A, idx, nextPos);
+            sum = sum + temp;
+            idx = nextPos;
+        }
+    }
+    return sum;
+}
