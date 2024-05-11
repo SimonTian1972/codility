9bd7c9b0861c868161d6a31e587de76404485df7
gary <ying050524@gmail.com>
2024-05-11 20:32:07 +0100

    comparator

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -22,56 +22,53 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
-int solution(vector<int>& A) {
-    const auto MAX_INTERSECTIONS = 10000000;
-    int intersections = 0;
-    int v_size = A.size();
 
-    // create a vector of vectors, each containing two integers representing a segment
-    vector<vector<int>> segments;
-    // reserve the memory for all the vectors to avoid many allocations
-    segments.reserve(v_size);
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-    // Add the pairs to the array
-    for (int i = 0; i < v_size; ++i) {
-        int radius = A[i];
-        int begin = max(0, static_cast<int>(i) - radius);
-        int end = min(v_size - 1, i + radius);
-        segments.push_back({ begin, end });
-    }
+// A C++ program to demonstrate STL stable_sort()
+// to sort intervals according to start time.
 
-    // sort the array by the first entry of each pair: the disk start indices
-    sort(segments.begin(), segments.end());
+using namespace std;
 
-    for (int i = 0; i < v_size; ++i) {
-        // for each disk in order of the *starting* position of the disk, not the centre
-        // find the end position of that disk from the array of vectors
-        int disk_end = segments[i][1];
-        // find the index of the rightmost value less than or equal to the interval-end
-        // this finds the number of disks that have started before disk i ends
-        //auto it = upper_bound(segments.begin(), segments.end(), vector<int>{disk_end, INT_MAX});
+// An interval has start time and end time
+struct Interval {
+    int start, end;
+};
 
-        auto it = upper_bound(segments.begin(), segments.end(), disk_end,
-            [](const int lhs, const vector<int>& rhs) -> bool { return lhs < rhs[0]; });
-
-        int count = distance(segments.begin(), it);
-        cout << "count= " << count << endl;
-
-        // subtract current position to exclude previous matches
-        // for disk i, i disks that start to the left have already been dealt with
-        // subtract i from count to prevent double counting
-        // subtract one more to prevent counting the disk itself
-        count -= (i + 1);
-        intersections += count;
-        if (intersections > MAX_INTERSECTIONS) {
-            return -1;
-        }
-    }
-    return intersections;
+// Compares two intervals according to starting
+// times.
+bool compareInterval(Interval i1, Interval i2)
+{
+    return (i1.start < i2.start);
 }
 
-int main() {
-    vector<int> A = { 1, 5, 2, 1, 4, 0 };
-    cout << "Result: " << solution(A) << endl;
+//bool compareInterval(Interval i1, Interval i2)
+//{
+//    return (i1.start < i2.start);
+//}  not correct since it does meet strict weak ordering
+
+
+
+int main()
+{
+    // Given intervals are sorted according to end time
+    Interval arr[]
+        = { { 1, 3 }, { 2, 4 }, { 1, 7 }, { 2, 19 } };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // sort the intervals in increasing order of
+    // start time such that the start time intervals
+    // appear in same order as in input.
+    //stable_sort(arr, arr + n, compareInterval);
+    sort(arr, arr + n, compareInterval);
+
+    cout << "Intervals sorted by start time : \n";
+    for (int i = 0; i < n; i++)
+        cout << "[" << arr[i].start << ", " << arr[i].end
+        << "] ";
+
     return 0;
 }
