ad787890cb325ab80e67e62d1fde94d442005861
gary <ying050524@gmail.com>
2024-05-12 12:39:13 +0100

    2015 socks

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -22,53 +22,45 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
+int solution(int K, vector<int>& C, vector<int>& D) {
+    // Implement your solution here
+    const int N = C.size();
+    const int M = D.size();
+    unordered_map<int, int> cleanMap; // sock_num, count
+    int ret = 0;
+    for (int s : C) {
+        cleanMap[s]++;
+        if (cleanMap[s] == 2) {
+            cleanMap[s] = 0;
+            ret++;
+        }
+    }
+    unordered_map<int, int> dirtyMap;
+    for (int s : D) {
+        dirtyMap[s]++;
+    }
 
-#include <algorithm>
-#include <iostream>
-#include <vector>
-using namespace std;
+    for (auto& myPair : cleanMap) {
+        if (K == 0) {
+            break;
+        }
+        if (myPair.second == 1) {
+            if (dirtyMap.find(myPair.first) != dirtyMap.end()) {
+                ret++; // more pair
+                K--; // wash one
+                dirtyMap[myPair.first]--; // dirty one less
+            }
+        }
+    }
 
-// A C++ program to demonstrate STL stable_sort()
-// to sort intervals according to start time.
-
-using namespace std;
-
-// An interval has start time and end time
-struct Interval {
-    int start, end;
-};
-
-// Compares two intervals according to starting
-// times.
-bool compareInterval(Interval i1, Interval i2)
-{
-    return (i1.start < i2.start);
-}
-
-//bool compareInterval(Interval i1, Interval i2)
-//{
-//    return (i1.start < i2.start);
-//}  not correct since it does meet strict weak ordering
-
-
-
-int main()
-{
-    // Given intervals are sorted according to end time
-    Interval arr[]
-        = { { 1, 3 }, { 2, 4 }, { 1, 7 }, { 2, 19 } };
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    // sort the intervals in increasing order of
-    // start time such that the start time intervals
-    // appear in same order as in input.
-    //stable_sort(arr, arr + n, compareInterval);
-    sort(arr, arr + n, compareInterval);
-
-    cout << "Intervals sorted by start time : \n";
-    for (int i = 0; i < n; i++)
-        cout << "[" << arr[i].start << ", " << arr[i].end
-        << "] ";
-
-    return 0;
+    for (auto& myPair : dirtyMap) {
+        if (K < 2) {
+            break;
+        }
+        int count = myPair.second / 2;
+        count = min(count, K / 2);
+        ret = ret + count;
+        K = K - 2*count;
+    }
+    return ret;
 }
