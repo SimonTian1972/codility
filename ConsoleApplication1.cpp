f940658d61737d66683326b40658db58744a82b4
gary <ying050524@gmail.com>
2024-05-12 13:24:59 +0100

    2015 RectangleBuilderGreaterArea

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -22,45 +22,115 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
-int solution(int K, vector<int>& C, vector<int>& D) {
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+#include <map>
+#include <vector>
+#include <set>
+#include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int solution(vector<int>& A, int X) {
     // Implement your solution here
-    const int N = C.size();
-    const int M = D.size();
-    unordered_map<int, int> cleanMap; // sock_num, count
-    int ret = 0;
-    for (int s : C) {
-        cleanMap[s]++;
-        if (cleanMap[s] == 2) {
-            cleanMap[s] = 0;
-            ret++;
-        }
+    map<double, int> myMap; // len, count
+    for (auto& item : A) {
+        myMap[item]++;
     }
-    unordered_map<int, int> dirtyMap;
-    for (int s : D) {
-        dirtyMap[s]++;
+    for (auto& myPair : myMap) {
+        myMap[myPair.first] = myPair.second / 2; // 2 fences per length or width
     }
 
-    for (auto& myPair : cleanMap) {
-        if (K == 0) {
-            break;
-        }
-        if (myPair.second == 1) {
-            if (dirtyMap.find(myPair.first) != dirtyMap.end()) {
-                ret++; // more pair
-                K--; // wash one
-                dirtyMap[myPair.first]--; // dirty one less
-            }
+    set<double> mySet;
+    for (auto& myPair : myMap) {
+        if (myPair.second != 0) {
+            mySet.insert(myPair.first);
         }
     }
 
-    for (auto& myPair : dirtyMap) {
-        if (K < 2) {
-            break;
+
+    long long ret = 0;
+    for (auto& item : mySet) {
+        double width = item;
+        double height = X / width;
+
+        auto valid = mySet.lower_bound(height);
+        int start_count = 1;
+        int end_count = distance(valid, mySet.end());
+        if (width < height) {
+            ret += start_count * end_count;
         }
-        int count = myPair.second / 2;
-        count = min(count, K / 2);
-        ret = ret + count;
-        K = K - 2*count;
+        //cout << "width= " << width << " height= " << height << " end_count= " << end_count << endl;
     }
-    return ret;
+
+    if (ret > 1000000000) {
+        return -1;
+    }
+    else {
+        return static_cast<int>(ret);
+    }
+}
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+#include <map>
+#include <vector>
+#include <set>
+#include <climits>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int solution(vector<int>& A, int X) {
+    // Implement your solution here
+    map<double, int> myMap; // len, count
+    for (auto& item : A) {
+        myMap[item]++;
+    }
+    for (auto& myPair : myMap) {
+        myMap[myPair.first] = myPair.second / 2; // 2 fences per length or width
+    }
+
+    set<double> mySet;
+    for (auto& myPair : myMap) {
+        if (myPair.second != 0) {
+            mySet.insert(myPair.first);
+        }
+    }
+
+
+    long long ret = 0;
+    for (auto& item : mySet) {
+        double width = item;
+        double height = X / width;
+
+        auto valid = mySet.lower_bound(height);
+        int start_count = 1;
+        int end_count = distance(valid, mySet.end());
+        if (width < height) {
+            ret += start_count * end_count;
+        }
+        //cout << "width= " << width << " height= " << height << " end_count= " << end_count << endl;
+    }
+
+    if (ret > 1000000000) {
+        return -1;
+    }
+    else {
+        return static_cast<int>(ret);
+    }
 }
