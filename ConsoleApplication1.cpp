2a9c7b752160730fd04c9a8371e8885ff2d3655d
gary <ying050524@gmail.com>
2024-07-01 15:18:18 +0100

    str_cmp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -107,90 +107,76 @@ void init_array_one_2d(double** ar, int n, int m) {
 
 
 #include <riscv_vector.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <math.h>
-
-#define N 31
-
-float input[N] = { -0.4325648115282207, -1.6655843782380970, 0.1253323064748307,
-                  0.2876764203585489,  -1.1464713506814637, 1.1909154656429988,
-                  1.1891642016521031,  -0.0376332765933176, 0.3272923614086541,
-                  0.1746391428209245,  -0.1867085776814394, 0.7257905482933027,
-                  -0.5883165430141887, 2.1831858181971011,  -0.1363958830865957,
-                  0.1139313135208096,  1.0667682113591888,  0.0592814605236053,
-                  -0.0956484054836690, -0.8323494636500225, 0.2944108163926404,
-                  -1.3361818579378040, 0.7143245518189522,  1.6235620644462707,
-                  -0.6917757017022868, 0.8579966728282626,  1.2540014216025324,
-                  -1.5937295764474768, -1.4409644319010200, 0.5711476236581780,
-                  -0.3998855777153632 };
-
-float output_golden[N] = {
-    1.7491401329284098,  0.1325982188803279,  0.3252281811989881,
-    -0.7938091410349637, 0.3149236145048914,  -0.5272704888029532,
-    0.9322666565031119,  1.1646643544607362,  -2.0456694357357357,
-    -0.6443728590041911, 1.7410657940825480,  0.4867684246821860,
-    1.0488288293660140,  1.4885752747099299,  1.2705014969484090,
-    -1.8561241921210170, 2.1343209047321410,  1.4358467535865909,
-    -0.9173023332875400, -1.1060770780029008, 0.8105708062681296,
-    0.6985430696369063,  -0.4015827425012831, 1.2687512030669628,
-    -0.7836083053674872, 0.2132664971465569,  0.7878984786088954,
-    0.8966819356782295,  -0.1869172943544062, 1.0131816724341454,
-    0.2484350696132857 };
-
-float output[N] = {
-    1.7491401329284098,  0.1325982188803279,  0.3252281811989881,
-    -0.7938091410349637, 0.3149236145048914,  -0.5272704888029532,
-    0.9322666565031119,  1.1646643544607362,  -2.0456694357357357,
-    -0.6443728590041911, 1.7410657940825480,  0.4867684246821860,
-    1.0488288293660140,  1.4885752747099299,  1.2705014969484090,
-    -1.8561241921210170, 2.1343209047321410,  1.4358467535865909,
-    -0.9173023332875400, -1.1060770780029008, 0.8105708062681296,
-    0.6985430696369063,  -0.4015827425012831, 1.2687512030669628,
-    -0.7836083053674872, 0.2132664971465569,  0.7878984786088954,
-    0.8966819356782295,  -0.1869172943544062, 1.0131816724341454,
-    0.2484350696132857 };
-
-void saxpy_golden(size_t n, const float a, const float* x, float* y) {
-    for (size_t i = 0; i < n; ++i) {
-        y[i] = a * x[i] + y[i];
-    }
-}
-
-// reference https://github.com/riscv/riscv-v-spec/blob/master/example/saxpy.s
-void saxpy_vec(size_t n, const float a, const float* x, float* y) {
-    for (size_t vl; n > 0; n -= vl, x += vl, y += vl) {
-        vl = __riscv_vsetvl_e32m8(n);
-        vfloat32m8_t vec_x = __riscv_vle32_v_f32m8(x, vl);
-        vfloat32m8_t vec_y = __riscv_vle32_v_f32m8(y, vl);
-        vec_x = __riscv_vfmul_vf_f32m8(vec_x, a, vl);
-        vec_y = __riscv_vfadd_vv_f32m8(vec_x, vec_y, vl);
-        __riscv_vse32_v_f32m8(y, vec_y, vl);
-    }
-}
-
-int fp_eq(float reference, float actual, float relErr)
-{
-    // if near zero, do absolute error instead.
-    float absErr = relErr * ((fabsf(reference) > relErr) ? fabsf(reference) : relErr);
-    return fabsf(actual - reference) < absErr;
-}
+#include <string.h>
 
 
-int main() {
-    saxpy_golden(N, 55.66, input, output_golden);
-    saxpy_vec(N, 55.66, input, output);
-    int pass = 1;
-    for (int i = 0; i < N; i++) {
-        if (!fp_eq(output_golden[i], output[i], 1e-6)) {
-            printf("fail, %f=!%f\n", output_golden[i], output[i]);
-            pass = 0;
+// reference https://github.com/riscv/riscv-v-spec/blob/master/example/strcmp.s
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+int strcmp_vec(const char* source1, const char* source2) {
+    size_t vlmax = __riscv_vsetvlmax_e8m8();
+    while (true) {
+        size_t vl = vlmax;
+        vuint8m8_t vec_1 = __riscv_vle8ff_v_u8m8(source1, &vl, vl);
+        vbool1_t mask1 = __riscv_vmseq_vx_u8m8_b1(vec_1, 0, vl);
+        int len1;
+        if (__riscv_vfirst_m_b1(mask1, vl) < 0) {
+            len1 = vl;
+        }
+        else {
+            len1 = __riscv_vfirst_m_b1(mask1, vl);
+        }
+
+        vuint8m8_t vec_2 = __riscv_vle8ff_v_u8m8(source2, &vl, vl);
+        vbool1_t mask2 = __riscv_vmseq_vx_u8m8_b1(vec_2, 0, vl);
+        int len2;
+        if (__riscv_vfirst_m_b1(mask2, vl) < 0) {
+            len2 = vl;
+        }
+        else {
+            len2 = __riscv_vfirst_m_b1(mask2, vl);
+        }
+
+        vbool1_t mask12 = __riscv_vmsne_vv_u8m8_b1(vec_1, vec_2, min(len1, len2));
+        int pos_diff = __riscv_vfirst_m_b1(mask12, min(len1, len2));
+
+        if (pos_diff >= 0) {
+            return (int)source1[pos_diff] - (int)source2[pos_diff];
+        }
+        else {
+            if (len1 == len2) {
+                source1 += len1;
+                source2 += len2;
+                if (__riscv_vfirst_m_b1(mask2, vl) >= 0) {
+                    return 0;
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                return len1 - len2;
+            }
         }
     }
-    if (pass)
-        printf("pass\n");
-    return (pass == 0);
 }
 
+int main() {
+    const int N = 1023;
+    const uint32_t seed = 0xdeadbeef;
+    srand(seed);
 
+    // data gen
+    char s0[N], s1[N];
+    gen_string(s0, N);
+    gen_string(s1, N);
+
+    // compute
+    int golden, actual;
+    golden = strcmp(s0, s1);
+    actual = strcmp_vec(s0, s1);
+
+    // compare
+    puts(golden == actual ? "pass" : "fail");
+}
 
