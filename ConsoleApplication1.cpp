363de4fe5da4d376da47a516b599a1a726ba3352
gary <ying050524@gmail.com>
2024-07-01 16:19:27 +0100

    str_len

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,44 +1,43 @@
 ﻿#include "common.h"
-#include <assert.h>
 #include <riscv_vector.h>
 #include <string.h>
 
-// reference https://github.com/riscv/riscv-v-spec/blob/master/example/strcpy.s
-char* strcpy_vec(char* destination, const char* source) {
-    signed char* src = source, * dst = destination;
+// reference https://github.com/riscv/riscv-v-spec/blob/master/example/strlen.s
+size_t strlen_vec(char* source) {
     size_t vlmax = __riscv_vsetvlmax_e8m8();
     size_t vl;
+    size_t count = 0;
     while (true) {
-        vint8m8_t vec_src = __riscv_vle8ff_v_i8m8(src, &vl, vlmax);
+        vint8m8_t vec_src = __riscv_vle8ff_v_i8m8(source, &vl, vlmax);
         vbool1_t mask = __riscv_vmseq_vx_i8m8_b1(vec_src, 0, vl);
         int pos = __riscv_vfirst_m_b1(mask, vl);
         if (pos == -1) {
-            __riscv_vse8_v_i8m8(dst, vec_src, vl);
-            src += vl;
-            dst += vl;
+            count = count + vl;
+            source = source + vl;
         }
         else {
-            __riscv_vse8_v_i8m8(dst, vec_src, pos + 1);
+            count = count + pos;
             break;
         }
     }
-    return destination;
+    return count;
 }
 
 int main() {
-    const int N = 2000;
     const uint32_t seed = 0xdeadbeef;
     srand(seed);
+
+    int N = rand() % 2000;
 
     // data gen
     char s0[N];
     gen_string(s0, N);
 
     // compute
-    char golden[N], actual[N];
-    strcpy(golden, s0);
-    strcpy_vec(actual, s0);
+    size_t golden, actual;
+    golden = strlen(s0);
+    actual = strlen_vec(s0);
 
     // compare
-    puts(strcmp(golden, actual) == 0 ? "pass" : "fail");
+    puts(golden == actual ? "pass" : "fail");
 }
