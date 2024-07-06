aa69bf1be31b7b3bf8bf2407a658af6a046aed6f
gary <ying050524@gmail.com>
2024-07-06 16:10:45 +0100

    maximumEnergy

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,111 +4,106 @@
 #include <string.h>
 #include <math.h>
 
-#define N 32
 
-#define MAX_BLOCKSIZE 32
-#define MLEN 4
-#define KLEN 8
-#define NLEN 4
-#define OUTPUT_LEN 16
+#include <stdio.h>
+#include <limits.h>  // For INT_MIN
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
-float a_array[MAX_BLOCKSIZE] = { -0.4325648115282207, -1.6655843782380970,
-                                0.1253323064748307,  0.2876764203585489,
-                                -1.1464713506814637, 1.1909154656429988,
-                                1.1891642016521031,  -0.0376332765933176,
-                                0.3272923614086541,  0.1746391428209245,
-                                -0.1867085776814394, 0.7257905482933027,
-                                -0.5883165430141887, 2.1831858181971011,
-                                -0.1363958830865957, 0.1139313135208096,
-                                1.0667682113591888,  0.0592814605236053,
-                                -0.0956484054836690, -0.8323494636500225,
-                                0.2944108163926404,  -1.3361818579378040,
-                                0.7143245518189522,  1.6235620644462707,
-                                -0.6917757017022868, 0.8579966728282626,
-                                1.2540014216025324,  -1.5937295764474768,
-                                -1.4409644319010200, 0.5711476236581780,
-                                -0.3998855777153632, 0.1 };
+int maxRiscv(int* energy, int energySize, int k) {
+    int ret = INT_MIN;
+    int n = k;
+    size_t vl;
+    for (int i = 0; i < k; i = i + vl, n = n - vl) {
+        size_t vl = __riscv_vsetvl_e32m1(n);
+        vint32m1_t vec_sum = __riscv_vmv_v_x_i32m1(0, vl);
+        for (int j = i; j < energySize; j = j + k) {
+            vint32m1_t vec_seq;
+            vec_seq = __riscv_vid_v_i32m1(vl);
+            vec_seq = __riscv_vadd_vx_i32m1(vec_seq, j, vl);
+            vbool32_t mask = __riscv_vmslt_vx_i32m1_b32(vec_seq, energySize, vl);
+            vint32m1_t vec_cur = __riscv_vmv_v_x_i32m1(0, vl);
+            size_t temp;
+            vec_cur = __riscv_vle32ff_v_i32m1_m(mask, energy + j, &temp, vl);
 
-float b_array[MAX_BLOCKSIZE] = { 1.7491401329284098,  0.1325982188803279,
-                                0.3252281811989881,  -0.7938091410349637,
-                                0.3149236145048914,  -0.5272704888029532,
-                                0.9322666565031119,  1.1646643544607362,
-                                -2.0456694357357357, -0.6443728590041911,
-                                1.7410657940825480,  0.4867684246821860,
-                                1.0488288293660140,  1.4885752747099299,
-                                1.2705014969484090,  -1.8561241921210170,
-                                2.1343209047321410,  1.4358467535865909,
-                                -0.9173023332875400, -1.1060770780029008,
-                                0.8105708062681296,  0.6985430696369063,
-                                -0.4015827425012831, 1.2687512030669628,
-                                -0.7836083053674872, 0.2132664971465569,
-                                0.7878984786088954,  0.8966819356782295,
-                                -0.1869172943544062, 1.0131816724341454,
-                                0.2484350696132857,  0.1 };
+            vbool32_t maskPrev = __riscv_vmslt_vx_i32m1_b32(vec_sum, 0, vl);
+            mask = __riscv_vmand_mm_b32(mask, maskPrev, vl);
 
-float golden_array[OUTPUT_LEN];
-float c_array[OUTPUT_LEN];
-
-void sgemm_golden() {
-    for (size_t i = 0; i < MLEN; ++i)
-        for (size_t j = 0; j < NLEN; ++j)
-            for (size_t k = 0; k < KLEN; ++k)
-                golden_array[i * NLEN + j] += a_array[i * KLEN + k] * b_array[j + k * NLEN];
-}
-
-
-// reference https://github.com/riscv/riscv-v-spec/blob/master/example/sgemm.S
-// c += a*b (alpha=1, no transpose on input matrices)
-// matrices stored in C row-major order
-void sgemm_vec(size_t size_m, size_t size_n, size_t size_k,
-    const float* a, // m * k matrix
-    size_t lda,
-    const float* b, // k * n matrix
-    size_t ldb,
-    float* c, // m * n matrix
-    size_t ldc) {
-
-    for (size_t i = 0; i < size_m; ++i) {
-        size_t vl;
-        size_t n = size_n;
-        for (size_t j = 0; j < size_n; j = j + vl, n = n - vl) {
-            vl = __riscv_vsetvl_e32m8(n);
-            vfloat32m8_t vec_sum = __riscv_vle32_v_f32m8(c + i * ldc + j, vl);
-
-            for (size_t k = 0; k < size_k; ++k) {
-                float ik = a[i * lda + k];
-                vfloat32m8_t vec_kj = __riscv_vle32_v_f32m8(b + k * ldb + j, vl);
-                vec_sum = __riscv_vfmacc_vf_f32m8(vec_sum, ik, vec_kj, vl);
-            }
-            printf("here j= %zu vl= %zu n= %zu  \n", j, vl, n);
-            __riscv_vse32_v_f32m8(c + i * ldc + j, vec_sum, vl);
+            // Set elements to zero based on the mask
+            vec_sum = __riscv_vmerge_vxm_i32m1(vec_sum, 0, mask, vl);
+            vec_sum = __riscv_vadd_vv_i32m1(vec_sum, vec_cur, vl);
         }
+        vint32m1_t vec_scalar = __riscv_vmv_v_x_i32m1(INT_MIN, vl);
+        vec_sum = __riscv_vredmax_vs_i32m1_i32m1(vec_sum, vec_scalar, vl);
+        int curMax = __riscv_vmv_x_s_i32m1_i32(vec_sum);
+        ret = max(ret, curMax);
     }
+    return ret;
 }
 
-int fp_eq(float reference, float actual, float relErr)
-{
-    // if near zero, do absolute error instead.
-    float absErr = relErr * ((fabsf(reference) > relErr) ? fabsf(reference) : relErr);
-    return fabsf(actual - reference) < absErr;
+int maximumEnergy(int* energy, int energySize, int k) {
+    int sumMax = INT_MIN;
+    for (int i = 0; i < k && i < energySize; i++) {
+        int sum = 0;
+        for (int j = i; j < energySize; j = j + k) {
+            sum = sum + energy[j];
+            if (j + k >= energySize) {
+                break;
+            }
+            else if (sum < 0) {
+                sum = 0;
+            }
+        }
+        sumMax = max(sumMax, sum);
+    }
+    return sumMax;
+}
+
+#include "assert.h"
+
+void testMaximumEnergy() {
+    // Test case 1
+    int energy1[] = { 1, -2, 3, 10, -4, 7, 2, -5 };
+    int energySize1 = sizeof(energy1) / sizeof(energy1[0]);
+    int k1 = 3;
+    int result1 = maximumEnergy(energy1, energySize1, k1);
+    int result1a = maxRiscv(energy1, energySize1, k1);
+    printf("Test case 1: The maximum energy is: %d %d\n", result1, result1a);
+
+    // Test case 2
+    int energy2[] = { -1, -2, -3, -4, -5, -6, -7, -8 };
+    int energySize2 = sizeof(energy2) / sizeof(energy2[0]);
+    int k2 = 2;
+    int result2 = maximumEnergy(energy2, energySize2, k2);
+    int result2a = maxRiscv(energy2, energySize2, k2);
+    printf("Test case 2: The maximum energy is: %d %d\n", result2, result2a);
+
+    // Test case 3
+    int energy3[] = { 5, -1, 3, -2, 4, -3, 2, -4, 1 };
+    int energySize3 = sizeof(energy3) / sizeof(energy3[0]);
+    int k3 = 4;
+    int result3 = maximumEnergy(energy3, energySize3, k3);
+    int result3a = maxRiscv(energy3, energySize3, k3);
+    printf("Test case 3: The maximum energy is: %d %d\n", result3, result3a);
+
+    // Test case 4
+    int energy4[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int energySize4 = sizeof(energy4) / sizeof(energy4[0]);
+    int k4 = 5;
+    int result4 = maximumEnergy(energy4, energySize4, k4);
+    int result4a = maxRiscv(energy4, energySize4, k4);
+    printf("Test case 4: The maximum energy is: %d %d\n", result4, result4a);
+
+    // Test case 5
+    int energy5[] = { 10, -10, 20, -20, 30, -30, 40, -40, 50, -50 };
+    int energySize5 = sizeof(energy5) / sizeof(energy5[0]);
+    int k5 = 6;
+    int result5 = maximumEnergy(energy5, energySize5, k5);
+    int result5a = maxRiscv(energy5, energySize5, k5);
+    printf("Test case 5: The maximum energy is: %d %d\n", result5, result5a);
 }
 
 int main() {
-    // golden
-    memcpy(golden_array, b_array, OUTPUT_LEN * sizeof(float));
-    sgemm_golden();
-    // vector
-    memcpy(c_array, b_array, OUTPUT_LEN * sizeof(float));
-    sgemm_vec(MLEN, NLEN, KLEN, a_array, KLEN, b_array, NLEN, c_array, NLEN);
-
-    int pass = 1;
-    for (int i = 0; i < OUTPUT_LEN; i++) {
-        if (!fp_eq(golden_array[i], c_array[i], 1e-5)) {
-            printf("index %d fail, %f=!%f\n", i, golden_array[i], c_array[i]);
-            pass = 0;
-        }
-    }
-    if (pass)
-        printf("pass\n");
-    return (pass == 0);
+    testMaximumEnergy();
+    return 0;
 }
+
